87454ed130fb88af005c6df94dfa4598679198f3
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-07-25 01:07:31 +0530

    Server Update.

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
-
+// App Config
 const PORT = process.env.PORT || 4000
 const app = express();
 await connectDB()
 
-// Intialize Middlewares
+
 app.use(express.json())
 app.use(cors())
 
