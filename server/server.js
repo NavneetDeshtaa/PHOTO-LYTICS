91c8eb912db330f8d1955ef43199748e158e5baf
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-07-25 01:07:13 +0530

    Server Side Update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import userRouter from './routes/userRoutes.js';
 import connectDB from './configs/mongodb.js';
 import imageRouter from './routes/imageRoutes.js';
 
-// App Config
+
 const PORT = process.env.PORT || 4000
 const app = express();
 await connectDB()
