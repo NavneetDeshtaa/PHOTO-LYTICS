f42d982342b1877a3bdca21157a8c86591ce8dd5
Navneet Deshta <navneetdeshtaa@gmail.com>
2025-07-25 01:22:58 +0530

    MongoDB Connection Updated.

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => {
-        console.log("Database Connected");
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/ai-image`)
-
+   try {  
+     const mongoURI = `${process.env.MONGODB_URI}/ai-image`;
+     await mongoose.connect(mongoURI);
+     console.log("Database Connected");
+    } 
+    catch (error) {
+         console.log(error);
+    }
 }
 
 export default connectDB;
